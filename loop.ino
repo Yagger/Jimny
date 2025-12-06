12b07c4416ba87e68059eae904d6264faa2d93f1
Kristjan Pihus <kpihus@gmail.com>
2025-12-06 10:04:27 +0200

    WALL RAMPING REMOVED

--- loop.ino
+++ loop.ino
@@ -83,51 +83,52 @@ void loop() {
       steering_offset = 0;
       RemoteXY.no_obstacles = 1;
       ramp_left = ramp_right = 0;
-      fullSteamAhead(wall_left, wall_right);
+      //fullSteamAhead(wall_left, wall_right);
     } else if (L && !C && R) { //Obstacle at left and right, squeeze from middle
       RemoteXY.obs_lr = 1;
       steering_offset = 0;
-      ramp_left = ramp_right = 0;
-      fullSteamAhead(wall_left, wall_right);
+      //ramp_left = ramp_right = 0;
+      //fullSteamAhead(wall_left, wall_right);
     } else if (L && C && !R) { // Obstacle leftish, overtake from right
       RemoteXY.obs_lc = 1;
       steering_offset = -hard_turn;
-      if(ramp_right < 30) ramp_right++;
-      if(ramp_left < 30) ramp_left++;
-      fullSteamAhead(wall_left - (400 * ramp_right/30), wall_right + (400 * ramp_left/30));
+      //if(ramp_right < 30) ramp_right++;
+     // if(ramp_left < 30) ramp_left++;
+      //fullSteamAhead(wall_left - (400 * ramp_right/30), wall_right + (400 * ramp_left/30));
     } else if (L && !C && !R) { // Obstacle far left, overtake from  little right
       RemoteXY.obs_l = 1;
       steering_offset = -slight_turn;
-      if(ramp_right < 20) ramp_right++;
-      if(ramp_left < 20) ramp_left++;
-      fullSteamAhead(wall_left - (300 * ramp_right/20), wall_right + (300 * ramp_left/20));
+      //if(ramp_right < 20) ramp_right++;
+      //if(ramp_left < 20) ramp_left++;
+      //fullSteamAhead(wall_left - (300 * ramp_right/20), wall_right + (300 * ramp_left/20));
     } else if (!L && C && R) { //Obstacle slight rightish, overtake from little left
       RemoteXY.obs_cr = 1;
       steering_offset = hard_turn;
-      if(ramp_right < 30) ramp_right++;
-      if(ramp_left < 30) ramp_left++;
-      fullSteamAhead(wall_left + (400 * ramp_left/30), wall_right - (400 * ramp_right/30));
+      //if(ramp_right < 30) ramp_right++;
+      //if(ramp_left < 30) ramp_left++;
+      //fullSteamAhead(wall_left + (400 * ramp_left/30), wall_right - (400 * ramp_right/30));
     } else if (!L && !C && R) { //Obstacle far right, overtake from little right
       RemoteXY.obs_r = 1;
       steering_offset = slight_turn;
-      if(ramp_right < 20) ramp_right++;
-      if(ramp_left < 20) ramp_left++;
-      fullSteamAhead(wall_left + (300 * ramp_left/20), wall_right - (300 * ramp_right/20));
+      //if(ramp_right < 20) ramp_right++;
+      //if(ramp_left < 20) ramp_left++;
+      //fullSteamAhead(wall_left + (300 * ramp_left/20), wall_right - (300 * ramp_right/20));
     } else { // Obstacle in the middle or everywhere (all three front sensors)
       if (wall_left > wall_right) { // More room at left, overtake from left
         RemoteXY.more_room_left = 1;
         steering_offset = hard_turn;
-        if(ramp_right < 30) ramp_right++;
-        if(ramp_left < 30) ramp_left++;
-        fullSteamAhead(wall_left + (400 * ramp_left/30), wall_right - (400 * ramp_right/30));
+        //if(ramp_right < 30) ramp_right++;
+        //if(ramp_left < 30) ramp_left++;
+        //fullSteamAhead(wall_left + (400 * ramp_left/30), wall_right - (400 * ramp_right/30));
       } else { //More room at right, overtake from right
         RemoteXY.more_room_right = 1;
         steering_offset = hard_turn;
-        if(ramp_right < 30) ramp_right++;
-        if(ramp_left < 30) ramp_left++;
-        fullSteamAhead(wall_left - (400 * ramp_right/30), wall_right + (400 * ramp_left/30));
+        //if(ramp_right < 30) ramp_right++;
+        //if(ramp_left < 30) ramp_left++;
+        //fullSteamAhead(wall_left - (400 * ramp_right/30), wall_right + (400 * ramp_left/30));
       }
-    } 
+    }
+    fullSteamAhead(wall_left, wall_right);
   }
   // Serial.printf("WR:%6d  R:%6d  C:%6d  L:%6d  WL:%6d MODE:%6d\n", wall_right, right, center, left, wall_left, RemoteXY.mode);
   // Serial.printf("WR:%d,R:%d,C:%d,L:%d,WL:%d\n", wall_right, right, center, left, wall_left);
